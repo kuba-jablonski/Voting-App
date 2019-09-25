1b42869007fc13441a0a730e82efa4c4f1de803d
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-09-25 03:56:14 +0200

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from "vue";
+import firebase from "firebase";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4udwa29H-PtXs182yJoc9xWK9X7G3qqs",
+  authDomain: "vue-vote.firebaseapp.com",
+  databaseURL: "https://vue-vote.firebaseio.com",
+  projectId: "vue-vote",
+  storageBucket: "vue-vote.appspot.com",
+  messagingSenderId: "977007321934",
+  appId: "1:977007321934:web:3da7a71bd727fb70e8741f",
+  measurementId: "G-4DLYW0C3D0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
