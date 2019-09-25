5e35dc954dc717b2dcbb414cab5a7fe40d662938
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-09-25 03:28:42 +0200

    create signin forms

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,68 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar flat app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>2POLL</span>
+        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="mr-3">
+        <v-btn text>
+          <span>About</span>
+        </v-btn>
+        <v-btn text>
+          <span>Poll list</span>
+        </v-btn>
+      </v-toolbar-items>
+      <!-- <v-btn outlined>Sign in</v-btn> -->
+      <v-dialog v-model="dialog" max-width="600px">
+        <template v-slot:activator="{ on }">
+          <v-btn outlined v-on="on">
+            Sign in
+          </v-btn>
+        </template>
+
+        <v-sheet>
+          <v-container>
+            Hello world!
+          </v-container>
+          <v-tabs grow>
+            <v-tab>Sign in</v-tab>
+            <v-tab>Register</v-tab>
+
+            <v-tab-item>
+              <v-container>
+                <login-form />
+              </v-container>
+            </v-tab-item>
+            <v-tab-item>
+              <v-container>
+                <signup-form />
+              </v-container>
+            </v-tab-item>
+          </v-tabs>
+        </v-sheet>
+      </v-dialog>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <!-- <HelloWorld /> -->
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import LoginForm from "@/components/LoginForm";
+import SignupForm from "@/components/SignupForm";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    LoginForm,
+    SignupForm
   },
   data: () => ({
-    //
+    dialog: false
   })
 };
 </script>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <v-text-field v-model="email" label="E-mail" required></v-text-field>
+    <v-text-field v-model="password" label="Password" required></v-text-field>
+    <v-btn @click="submit">submit</v-btn>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.email, this.password);
+    }
+  }
+};
+</script>
