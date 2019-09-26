90314548263dcbe84ec807e26a776b538ca52093
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-09-26 23:41:12 +0200

    move signin dialog to own component

---
+++ src/components/SigninDialog.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-dialog :value="open" @input="$emit('close')" max-width="600px">
+    <v-sheet>
+      <v-container>
+        <v-row align="center" justify="center" class="mb-4">
+          <v-img :src="logo" alt="Logo" max-height="100" max-width="100" />
+        </v-row>
+        <v-row justify="center" class="mb-5 font-weight-light">
+          Sign in with your social network account
+        </v-row>
+        <v-row
+          justify="space-around"
+          class="mb-5 "
+          :style="{ maxWidth: '450px', margin: '0 auto' }"
+        >
+          <icon-base height="40" width="40">
+            <icon-google />
+          </icon-base>
+          <icon-base height="40" width="40">
+            <icon-facebook />
+          </icon-base>
+          <icon-base height="40" width="40">
+            <icon-twitter />
+          </icon-base>
+          <icon-base height="40" width="40">
+            <icon-github />
+          </icon-base>
+        </v-row>
+        <v-row justify="center" class="font-weight-light mb-5">
+          Or use your email
+        </v-row>
+
+        <v-tabs grow>
+          <v-tab>Sign in</v-tab>
+          <v-tab>Register</v-tab>
+
+          <v-tab-item>
+            <v-container>
+              <login-form />
+            </v-container>
+          </v-tab-item>
+          <v-tab-item>
+            <v-container>
+              <signup-form />
+            </v-container>
+          </v-tab-item>
+        </v-tabs>
+      </v-container>
+    </v-sheet>
+  </v-dialog>
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm";
+import SignupForm from "@/components/SignupForm";
+import IconBase from "@/components/icons/IconBase";
+import IconGoogle from "@/components/icons/IconGoogle";
+import IconFacebook from "@/components/icons/IconFacebook";
+import IconTwitter from "@/components/icons/IconTwitter";
+import IconGithub from "@/components/icons/IconGithub";
+
+export default {
+  props: ["open"],
+  components: {
+    LoginForm,
+    SignupForm,
+    IconBase,
+    IconGoogle,
+    IconFacebook,
+    IconTwitter,
+    IconGithub
+  },
+  data() {
+    return {
+      logo: require("@/assets/poll-icon-4.jpg")
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ src/views/Polls.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-list>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group v-model="item">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-icon>
+            <v-chip>Default</v-chip>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"> </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-list-item-action-text
+              >by bob | 2019/09/09</v-list-item-action-text
+            >
+          </v-list-item-action>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    item: 1,
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Audience", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" }
+    ]
+  })
+};
+</script>
+
+<style></style>
