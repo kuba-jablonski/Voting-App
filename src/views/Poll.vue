761679bdc1bf29faeecc30fdde84a6f1020ffb42
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-11-13 00:43:15 +0100

    split data fetching logic

--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -43,34 +43,41 @@ export default {
       return this.voters.includes(this.uid) ? "poll-results" : "poll-vote";
     }
   },
-  mounted() {
-    firebase
-      .firestore()
-      .collection("polls")
-      .doc(`${this.$route.params.id}`)
-      .get()
-      .then(doc => {
-        const { author, question, voters } = doc.data();
-        this.question = question;
-        this.author = author;
-        this.voters = voters;
-      });
-
-    firebase
-      .firestore()
-      .collection("polls")
-      .doc(`${this.$route.params.id}`)
-      .collection("options")
-      .get()
-      .then(snap => {
-        snap.forEach(doc => {
-          console.log(doc.data());
-          this.options.push({
-            id: doc.id,
-            ...doc.data()
-          });
+  methods: {
+    async fetchPoll() {
+      console.log("fetching POLL");
+      const doc = await firebase
+        .firestore()
+        .collection("polls")
+        .doc(`${this.$route.params.id}`)
+        .get();
+      const { author, question, voters } = doc.data();
+      this.question = question;
+      this.author = author;
+      this.voters = voters;
+      console.log("fetching POLL - DONE");
+    },
+    async fetchOptions() {
+      console.log("fetching OPTIONS");
+      const snap = await firebase
+        .firestore()
+        .collection("polls")
+        .doc(`${this.$route.params.id}`)
+        .collection("options")
+        .get();
+      snap.forEach(doc => {
+        console.log(doc.data());
+        this.options.push({
+          id: doc.id,
+          ...doc.data()
         });
       });
+      console.log("fetching OPTIONS - DONE");
+    }
+  },
+  mounted() {
+    this.fetchPoll();
+    this.fetchOptions();
   }
 };
 </script>
