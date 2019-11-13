44e46ca0e6cc30bd047c4bb6f0bb80e49fcdab25
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-11-13 06:39:43 +0100

    add a homepage

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <h1 class="mt-5 mb-5 display-2 font-weight-thin">
+      Poll simple. Poll everywhere.
+    </h1>
+    <p class="font-weight-light">
+      Ask a question, then watch <span class="primary--text">LIVE</span> result.
+    </p>
+    <p class="font-weight-light">
+      Create public and private polls, free, unlimited, always.
+    </p>
+    <v-btn depressed color="primary" class="mr-3">Get Started</v-btn>
+    <v-btn depressed color="white">View poll list</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
