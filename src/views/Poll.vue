e8def4a013511750c09962314618840ce348ab55
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-12-04 18:05:36 +0100

    Combine Poll and PollResults

--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -8,29 +8,46 @@
       <v-btn depressed class="my-5 ">
         <v-icon left dark>mdi-chevron-left</v-icon>Back to list
       </v-btn>
-      <poll-results
-        :options="options"
-        :question="poll.question"
-        :loaded="loaded"
-      />
+      <div>
+        <div class="option" v-for="option in options" :key="option.option">
+          <span>{{ option.option }}</span>
+          <div class="bar white--text" ref="bars">
+            {{ option.count }}
+          </div>
+        </div>
+      </div>
     </div>
   </v-container>
 </template>
 
 <script>
 import { db } from "@/main";
-import PollResults from "@/components/PollResults";
 
 export default {
-  components: { PollResults },
   data() {
     return {
       poll: null,
-      options: [],
-      loaded: false
+      options: []
     };
   },
+  computed: {
+    highestCount() {
+      return Math.max(...this.options.map(o => o.count));
+    }
+  },
   methods: {
+    animate(ref, targetWidth) {
+      if (targetWidth < 1) return;
+      let width = 0;
+      let id = setInterval(() => {
+        if (width >= targetWidth) {
+          clearInterval(id);
+        } else {
+          width++;
+          ref.style.width = width + "%";
+        }
+      }, 5);
+    },
     async fetchPoll() {
       const doc = await db
         .collection("polls")
@@ -54,7 +71,23 @@ export default {
   },
   async mounted() {
     await Promise.all([this.fetchPoll(), this.fetchOptions()]);
-    this.loaded = true;
+    this.options.forEach((o, i) => {
+      this.animate(this.$refs.bars[i], (o.count / this.highestCount) * 100);
+    });
   }
 };
 </script>
+
+<style scoped>
+.bar {
+  height: 20px;
+  width: 20px;
+  background-color: red;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 6px;
+  margin-bottom: 20px;
+  font-size: 14px;
+}
+</style>
